51617a10b5176faedc8ac73b47e12945d6842792
Jonas Oliveira <jonasoliveira.webdeveloper@gmail.com>
2023-01-11 13:19:47 -0300

    initial project

---
+++ src/main/java/com/recanto/recanto/RecantoApplication.java
@@ -0,0 +1,13 @@
+package com.recanto.recanto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecantoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecantoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recanto/recanto/RecantoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recanto.recanto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecantoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
