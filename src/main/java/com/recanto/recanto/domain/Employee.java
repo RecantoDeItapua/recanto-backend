58a814148321534b0252a8c8646a0e842d019500
Jonas Oliveira <jonasoliveira.webdeveloper@gmail.com>
2023-01-17 10:55:36 -0300

    alter class

--- src/main/java/com/recanto/recanto/domain/Employee.java
+++ src/main/java/com/recanto/recanto/domain/Employee.java
@@ -3,31 +3,30 @@ package com.recanto.recanto.domain;
 import com.recanto.recanto.enums.Profile;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
-@Data
 @EqualsAndHashCode
 @AllArgsConstructor
-@Builder
 @Entity
+@Getter
+@Setter
 public class Employee extends Person{
 
-    @Getter @Setter
     @OneToMany(mappedBy = "employee")
     private List<ServiceProvider> providers = new ArrayList<>();
 
+    @OneToMany(mappedBy = "employee")
+    private List<Annoucements> annoucements = new ArrayList<>();
 
-    public Employee(UUID id, String name, String cpf, String email, String password) {
+
+    public Employee(Integer id, String name, String cpf, String email, String password) {
         super(id, name, cpf, email, password);
         addProfiles(Profile.CONCIERGE);
     }
--- src/main/java/com/recanto/recanto/domain/Resident.java
+++ src/main/java/com/recanto/recanto/domain/Resident.java
@@ -2,6 +2,8 @@ package com.recanto.recanto.domain;
 
 
 import com.recanto.recanto.enums.Profile;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,16 +11,22 @@ import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 @Entity
+@AllArgsConstructor
+@Getter
+@Setter
 public class Resident extends Person {
-    @Getter @Setter
+
     @OneToMany(mappedBy = "resident")
     private List<ServiceProvider> providers = new ArrayList<>();
 
-    public Resident(UUID id, String name, String cpf, String email, String password) {
+    @OneToMany(mappedBy = "resident")
+    private List<Annoucements> annoucements = new ArrayList<>();
+
+    public Resident(Integer id, String name, String cpf, String email, String password) {
         super(id, name, cpf, email, password);
+
     }
 
     public Resident() {
